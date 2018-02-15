be0833908b4223d4a2309d14ca8c08ead84d11ab
Igor Farias <igor.farias@ccc.ufcg.edu.br>
2018-02-15 14:43:15 -0300

    Added basic dish service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
