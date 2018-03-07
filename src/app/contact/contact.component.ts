b614bfef00fb5d564855bb7041f41f6be509abfb
Igor Farias <igor.farias@ccc.ufcg.edu.br>
2018-03-07 09:32:20 -0300

    Added reactive to contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -27,10 +27,16 @@ export class ContactComponent implements OnInit {
       telnum: 0,
       email: '',
       agree: false,
-      contactType: 'None',
+      contacttype: 'None',
       message: ''
 
     });
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
