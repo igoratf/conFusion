deb6c4ca23294f6bb8fd425f19f02aa9d7663a23
igoratf <igor.atf@outlook.com>
2018-07-02 23:44:33 -0300

    upgrade to angular 6

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable, interval, pipe } from 'rxjs';
 
 import { baseURL } from '../shared/baseurl';
 import { ProcessHttpmsgService } from './process-httpmsg.service';
+// Statics
+import 'rxjs/add/observable/throw';
 
-
+// Operators
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/delay';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+import {switchMap, map} from 'rxjs/operators';
+
 
 @Injectable()
 export class DishService {
@@ -17,27 +24,27 @@ export class DishService {
   constructor(private http: Http, private processHTTPMsgService: ProcessHttpmsgService) { }
 
   getDishes(): Observable<Dish[]> {
-    return this.http.get(baseURL + 'dishes').map(res => {
+    return this.http.get(baseURL + 'dishes').pipe(map(res => {
       return this.processHTTPMsgService.extractData(res);
-    });
+    }));
   }
 
   getDish(id: number): Observable<Dish> {
-    return this.http.get(baseURL + 'dishes/' + id).map(res => {
+    return this.http.get(baseURL + 'dishes/' + id).pipe(map(res => {
       return this.processHTTPMsgService.extractData(res);
-    });
+    }));
 }
 
   getFeaturedDish(): Observable<Dish> {
-    return this.http.get(baseURL + 'dishes?featured=true').map(res => {
+    return this.http.get(baseURL + 'dishes?featured=true').pipe(map(res => {
       return this.processHTTPMsgService.extractData(res)[0];
-    })
+    }));
 }
 
   getDishIds(): Observable<number[]> {
-    return this.getDishes().map(dishes => {
+    return this.getDishes().pipe(map(dishes => {
       return dishes.map(dish => dish.id);
-    })
+    }));
   }
 
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,14 +2,23 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
-
+import { Observable, interval, pipe } from 'rxjs';
 import { baseURL } from '../shared/baseurl';
 import { ProcessHttpmsgService } from '../services/process-httpmsg.service';
 
-import { Observable } from 'rxjs/Observable';
 
-import 'rxjs/add/operator/delay';
+// Statics
+import 'rxjs/add/observable/throw';
+
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+
+import {switchMap, map} from 'rxjs/operators';
 
 @Injectable()
 export class LeaderService {
@@ -18,21 +27,21 @@ export class LeaderService {
 
 
   getLeaders(): Observable<Leader[]> {
-    return this.http.get(baseURL + 'leaders').map(res => {
+    return this.http.get(baseURL + 'leaders').pipe(map(res => {
       return this.processHttpMsgService.extractData(res);
-    });
+    }));
   }
 
   getLeader(id: number): Observable<Leader> {
-    return this.http.get(baseURL + 'leaders/' + id).map(res => {
+    return this.http.get(baseURL + 'leaders/' + id).pipe(map(res => {
       return this.processHttpMsgService.extractData(res);
-    });
+    }));
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return this.http.get(baseURL + 'leaders?featured=true').map(res => {
+    return this.http.get(baseURL + 'leaders?featured=true').pipe(map(res => {
       return this.processHttpMsgService.extractData(res)[0];
-    });
+    }));
   }
 
 }
